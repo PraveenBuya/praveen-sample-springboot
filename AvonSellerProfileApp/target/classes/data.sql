4abe674ad69014490a08c783b993e37843dd7031
PraveenBuya <praveenbuya@gmail.com>
2016-11-05 08:29:56 -0700

    Delete data.sql

--- AvonSellerProfileApp/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-
-insert into  Seller values(1,'vinuthna','vinu','vinu.vinuthna1@gmail.com','9876541235');
-insert into  Seller values(2,'bramhini','b','bramhini.b@gmail.com','9836541235');
-insert into  Seller values(3,'shivani','s','shivani.s@gmail.com','9876541235');
